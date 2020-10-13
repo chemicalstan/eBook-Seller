7bafdded06ba48bf7bcd9724f8ce9216f1da2eb3
Chemical Stan <chemicalstan@Stanleys-MacBook-Pro.local>
2020-10-13 11:07:09 +0100

    middlewares set and index initialized

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require("express"),
   bodyParser = require("body-parser"),
   stripeSecretKey = require("./config/keys").stripeSecretKey,
-  stripe = require("stripe")(stripeSecretKey);
-exphbs = require("express-handlebars");
+  stripe = require("stripe")(stripeSecretKey),
+  exphbs = require("express-handlebars");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Handlebars Middleware
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// set static folder
+app.use(express.static(`${__dirname}/public`));
+
+// Routers
 app.use("/", (req, res) => {
-  res.end("Hello world");
+  res.render("index");
 });
 
 app.listen(port, () => {
-  console.log(`Application running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
